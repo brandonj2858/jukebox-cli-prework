dc60a92a1138b775c64d0cd3ccfbea93cd4b4ee5
Brandon James <brandonj2858@gmail.com>
2019-07-28 17:32:29 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -44,7 +44,28 @@ def help
  end
  
 def run
- 
+    # help
+
+  input = ""
+  while input
+    puts "Please enter a command:"
+    input = gets.downcase.strip
+    case input
+    when 'list'
+      list(songs)
+    when 'play'
+      list(songs)
+      play(songs)
+    when 'help'
+      help
+    when 'exit'
+      exit_jukebox
+      break
+    else
+      help
+    end
+  end
+end
 
  
  
