5a483ae222a89a1579cd0d8b6ff2aacf450cc99c
Brandon James <brandonj2858@gmail.com>
2019-07-28 16:18:59 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,6 +31,8 @@ def help
    song_to_play = gets.chomp
    if songs.include?(song_to_play)
      puts "Playing #{song_to_play}"
+   else if song_to_play.Class = Integer
+     puts "Playing #"
    else
      puts "Invalid input, please try again"
    end
