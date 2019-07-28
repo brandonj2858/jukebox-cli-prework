37b0638d2b9c09185f5050eba1cdc86c67ae6da6
Brandon James <brandonj2858@gmail.com>
2019-07-28 16:09:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,8 +27,6 @@ def help
  end
  
  def play(songs)
-   puts "Please enter a song name or number:" = gets.chomp
-   if songs.include(gets.chomp)
-   
-
+   puts "Please enter a song name or number:"
+   song_to_play = gets.chomp
 
